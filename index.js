8b1dd4d5c0653689ebea7127524d8d1e0fe894a6
Gabriel Schirmer <gabriel.schirmer@sou.unijui.edu.br>
2022-10-10 18:27:00 -0300

    refacotring some stuff

--- index.js
+++ index.js
@@ -16,19 +16,15 @@ const brightness = document.getElementById('brightness')
 const gamma = document.getElementById('gamma')
 
 originalImage.onload = () => {
-  // sets canvas size to fit images
   canvaoOriginal.width = originalImage.width
   canvaoOriginal.height = originalImage.height
   canvaoComFiltro.width = originalImage.width
   canvaoComFiltro.height = originalImage.height
 
-  // draw original image
   contextOriginal.drawImage(originalImage, 0, 0)
   contextComFiltro.drawImage(originalImage, 0, 0)
 }
 
-
-// Handle image upload into img tag
 imageLoader.addEventListener('change', function (e) {
   var reader = new FileReader();
 
@@ -59,31 +55,33 @@ function applyGauss() {
   for (var ch = 0; ch < 3; ch++) {
     gauss_internal(pixels, kernel, ch, false);
   }
-  // Apply the modified pixels
+
   contextComFiltro.putImageData(pixels, 0, 0);
 }
 
 /**
-* Gaussian
-* @param imagePixels - pixels do canvas
-* @param kernel - the Gaussian blur kernel
+* APROXIMACAO UNIDIMENSIONAL
+* @param pixels - pixels do canvas
+* @param kernel - Kernel do Gaussian Blur
 * @param colorChannel - canal de cores onde o blur sera aplicado
 * @param grayFlag - flag para mostrar o RGB
+* @param ch - canal de cores
+* @param kl - tamanho do kernel
 */
 function gauss_internal(pixels, kernel, ch, gray) {
-  var data = pixels.data;
-  var width = pixels.width;
-  var heigth = pixels.height;
-  var buff = new Uint8Array(width * heigth);
-  var mk = Math.floor(kernel.length / 2);
-  var kl = kernel.length;
+  const data = pixels.data;
+  const width = pixels.width;
+  const heigth = pixels.height;
+  let buff = new Uint8Array(width * heigth);
+  const mk = Math.floor(kernel.length / 2);
+  const kl = kernel.length;
 
-  // First step process columns
-  for (var j = 0, hw = 0; j < heigth; j++, hw += width) {
-    for (var i = 0; i < width; i++) {
-      var sum = 0;
-      for (var k = 0; k < kl; k++) {
-        var col = i + (k - mk);
+  // Processando colunas
+  for (let j = 0, hw = 0; j < heigth; j++, hw += width) {
+    for (let i = 0; i < width; i++) {
+      let sum = 0;
+      for (let k = 0; k < kl; k++) {
+        let col = i + (k - mk);
         col = (col < 0) ? 0 : ((col >= width) ? width - 1 : col);
         sum += data[(hw + col) * 4 + ch] * kernel[k];
       }
@@ -91,41 +89,47 @@ function gauss_internal(pixels, kernel, ch, gray) {
     }
   }
 
-  // Second step process rows
-  for (var j = 0, offset = 0; j < heigth; j++, offset += width) {
-    for (var i = 0; i < width; i++) {
-      var sum = 0;
+  // Processando linhas
+  for (let j = 0, offset = 0; j < heigth; j++, offset += width) {
+    for (let i = 0; i < width; i++) {
+      let sum = 0;
       for (k = 0; k < kl; k++) {
-        var row = j + (k - mk);
+        let row = j + (k - mk);
         row = (row < 0) ? 0 : ((row >= heigth) ? heigth - 1 : row);
         sum += buff[(row * width + i)] * kernel[k];
       }
-      var off = (j * width + i) * 4;
+      let off = (j * width + i) * 4;
       (!gray) ? data[off + ch] = sum :
         data[off] = data[off + 1] = data[off + 2] = sum;
     }
   }
 }
 
+
+/**
+* APROXIMACAO UNIDIMENSIONAL
+* Cria um array unidimensional, tamanho do filtro com seus coeficientes
+*/
 function makeGaussKernel(sigma) {
   const GAUSSKERN = 6.0;
-  var dim = parseInt(Math.max(3.0, GAUSSKERN * sigma));
-  var sqrtSigmaPi2 = Math.sqrt(Math.PI * 2.0) * sigma;
-  var s2 = 2.0 * sigma * sigma;
-  var sum = 0.0;
+  const dim = parseInt(Math.max(3.0, GAUSSKERN * sigma));
+  const sqrtSigmaPi2 = Math.sqrt(Math.PI * 2.0) * sigma;
+  const s2 = 2.0 * sigma * sigma;
+  let sum = 0.0;
 
-  var kernel = new Float32Array(dim - !(dim & 1)); // Make it odd number
+  let kernel = new Float32Array(dim - !(dim & 1)); // Transforma em impar
   const half = parseInt(kernel.length / 2);
-  for (var j = 0, i = -half; j < kernel.length; i++, j++) {
+  for (let j = 0, i = -half; j < kernel.length; i++, j++) {
     kernel[j] = Math.exp(-(i * i) / (s2)) / sqrtSigmaPi2;
     sum += kernel[j];
   }
-  // Normalize the gaussian kernel to prevent image darkening/brightening
-  for (var i = 0; i < dim; i++) {
+  // Normalizando o kernel para previnir brilho ou escurecimento da imagem
+  for (let i = 0; i < dim; i++) {
     kernel[i] /= sum;
   }
   return kernel;
 }
+
 // Laplace funcs
 function applyLaplace() {
   let pixels = contextComFiltro.getImageData(0, 0, canvaoOriginal.width, canvaoOriginal.height)
@@ -145,19 +149,19 @@ function applyLaplace() {
     canvasHeight = pixels.height,
     outputData = contextComFiltro.createImageData(canvasWidth, canvasHeight)
 
-  for (var y = 0; y < canvasHeight; y++) {
+  for (let y = 0; y < canvasHeight; y++) {
 
-    for (var x = 0; x < canvasWidth; x++) {
+    for (let x = 0; x < canvasWidth; x++) {
 
-      var dstOff = (y * canvasWidth + x) * 4,
+      let dstOff = (y * canvasWidth + x) * 4,
         sumReds = 0,
         sumGreens = 0,
         sumBlues = 0
 
-      for (var kernelY = 0; kernelY < side; kernelY++) {
-        for (var kernelX = 0; kernelX < side; kernelX++) {
+      for (let kernelY = 0; kernelY < side; kernelY++) {
+        for (let kernelX = 0; kernelX < side; kernelX++) {
 
-          var currentKernelY = y + kernelY - halfSide,
+          let currentKernelY = y + kernelY - halfSide,
             currentKernelX = x + kernelX - halfSide
 
           if (currentKernelY >= 0 &&
@@ -165,7 +169,7 @@ function applyLaplace() {
             currentKernelX >= 0 &&
             currentKernelX < canvasWidth) {
 
-            var offset = (currentKernelY * canvasWidth + currentKernelX) * 4,
+            let offset = (currentKernelY * canvasWidth + currentKernelX) * 4,
               weight = weights[kernelY * side + kernelX]
 
             sumReds += src[offset] * weight
@@ -185,9 +189,6 @@ function applyLaplace() {
 }
 
 function applyGrayScale(pixels) {
-  var width = pixels.width;
-  var height = pixels.height;
-
   let data = pixels.data
 
   for (let i = 0; i < data.length; i += 4) {
@@ -205,7 +206,7 @@ function applyGrayScale(pixels) {
 }
 
 //Brilho Funcs
-const applyBrilho = () => {
+function applyBrilho() {
   const imageData = contextComFiltro.getImageData(0, 0, canvaoOriginal.width, canvaoOriginal.height);
   const data = imageData.data;
 
@@ -213,7 +214,6 @@ const applyBrilho = () => {
     data[i] = data[i] + 100;
     data[i + 1] = data[i + 1] + 100;
     data[i + 2] = data[i + 2] + 100;
-    // data[i + 3] = 255;
   }
 
   contextComFiltro.putImageData(imageData, 0, 0);
